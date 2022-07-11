2a2593a2f37b487d9ca426e1a62934641c3672ad
Venceslav Dimitrov <venceslav_dimitrov@abv.bg>
2022-07-11 15:36:13 +0300

    changed nacl.tf

--- nacl.tf
+++ nacl.tf
@@ -1,24 +1,41 @@
+# Create default VPC Network ACL
 resource "aws_network_acl" "test-ACL" {
-  vpc_id = aws_vpc.Test-VPC.id
-}
-
-resource "aws_network_acl_rule" "inbound22" {
-  network_acl_id = aws_network_acl.test-ACL.id
-  rule_number    = 100
-  egress         = false
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-  from_port      = 22
-  to_port        = 22
-}
-resource "aws_network_acl_rule" "inbound80" {
-  network_acl_id = aws_network_acl.test-ACL.id
-  rule_number    = 200
-  egress         = false
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-  from_port      = 80
-  to_port        = 80
+    vpc_id = aws_vpc.Test-VPC.id
+    
+    egress {
+        protocol = "-1"
+        rule_no = 100
+        action = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port = 0
+        to_port = 0
+    }
+    ingress {
+        protocol = "tcp"
+        rule_no = 100
+        action = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port = 22
+        to_port = 22
+    }
+    ingress {
+        protocol = "tcp"
+        rule_no = 200
+        action = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port = 80
+        to_port = 80
+    }
+    ingress {
+        protocol = "tcp"
+        rule_no = 300
+        action = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port = 443
+        to_port = 443
+    }
+    subnet_ids = [
+        aws_subnet.public-network.id,
+        aws_subnet.private-network.id
+    ]
 }
