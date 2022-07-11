164a4d075a118b709818a406d329ebe7a924a807
Venceslav Dimitrov <venceslav_dimitrov@abv.bg>
2022-07-11 15:37:54 +0300

    added port 443 to publi-security-group

--- security_groups.tf
+++ security_groups.tf
@@ -16,6 +16,12 @@ resource "aws_security_group" "public-security-group" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port = 0
         to_port = 0
